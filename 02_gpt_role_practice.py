6aacc52319180fe719110e2c2109f53d2baea172
yjh111172 <adgj06747@gmail.com>
2025-09-16 12:28:18 +0900

    Update 02_gpt_role_practice.py

--- 02_gpt_role_practice.py
+++ 02_gpt_role_practice.py
@@ -9,7 +9,7 @@ client = OpenAI(api_key=api_key)
 
 response1 = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    temperature=0.9,
+    temperature=1,
     messages=[
         # {"role": "system", "content": "너는 백설공주 이야기 속 마법 거울이야. 그 이야기의 캐릭터에 맞게 답변해줘."},
         {"role": "system", "content": "너는 영화 배트맨에 나오는 조커 역할이야. 조커는 악당이야. 그 캐릭터에 맞게 답변해줘."},
